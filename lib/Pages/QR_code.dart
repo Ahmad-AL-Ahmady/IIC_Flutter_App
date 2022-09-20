cbd034297cf3ca86f15ce2dbd30b826de9108076
Ziyad-Seif <ziyad.seif@ibm.com>
2022-09-20 17:21:40 +0200

    QR

---
+++ lib/Pages/QR_code.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:login_app/Pages/dashboard.dart';
+import 'package:login_app/Pages/globels.dart';
+import 'package:login_app/Pages/registerationotp.dart';
+import 'package:login_app/Pages/registration2.dart';
+import 'package:login_app/Pages/resetpassotp.dart';
+import 'package:login_app/UI/custom_text_field.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+
+class QrCode extends StatefulWidget {
+  const QrCode({Key? key}) : super(key: key);
+
+  @override
+  State<QrCode> createState() => _QrCodeState();
+}
+
+class _QrCodeState extends State<QrCode> {
+  Image? image;
+
+  @override
+  void initState() {
+    super.initState();
+    Generate_qrcode();
+  }
+
+  Future<void> Generate_qrcode() async {
+    var response = await http.post(
+        Uri.https('iic-simple-toolchain-20220912122755303.mybluemix.net',
+            '/qr/generate'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': await getStringValuesSF()
+        },
+        body: jsonEncode({}));
+
+    var data = response.body;
+
+    print("======================");
+    print(data);
+    var awm = Image.memory(base64Decode(data));
+    if (response.statusCode == 200) {
+      setState(() {
+        image = awm;
+      });
+    } else {
+      print("ERROR");
+    }
+  }
+
+  getStringValuesSF() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+    return token;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Dashboard",
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        backgroundColor: Color.fromARGB(255, 2, 47, 98),
+        automaticallyImplyLeading: false,
+        leadingWidth: 200,
+        elevation: 0,
+        leading: ElevatedButton.icon(
+          onPressed: () async {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Dashboard()));
+            SharedPreferences prefs = await SharedPreferences.getInstance();
+            prefs.setString('token', "");
+          },
+          icon: const Icon(Icons.arrow_left_sharp),
+          label: const Text('Sign Out'),
+          style: ElevatedButton.styleFrom(
+              elevation: 0, primary: Colors.transparent),
+        ),
+      ),
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          child: Stack(
+            children: [
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                      Color.fromARGB(255, 0, 43, 91),
+                      Color.fromARGB(255, 43, 72, 101),
+                      Color.fromARGB(255, 37, 109, 133),
+                      Color.fromARGB(255, 143, 227, 207),
+                    ],
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 20),
+                  child: SingleChildScrollView(
+                    physics: BouncingScrollPhysics(),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 200,
+                        ),
+                        Container(
+                          child: Center(
+                            child: image,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/Pages/dashboard.dart
+++ lib/Pages/dashboard.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:login_app/Pages/QR_code.dart';
 import 'package:login_app/Pages/incedents.dart';
 import 'package:login_app/Pages/login.dart';
 import 'package:login_app/Pages/services.dart';
@@ -241,29 +242,34 @@ class _DashboardState extends State<Dashboard> {
                         SizedBox(
                           height: 5,
                         ),
-                        // Padding(
-                        //   padding: const EdgeInsets.symmetric(vertical: 25),
-                        //   child: Container(
-                        //     width: 250,
-                        //     child: RaisedButton(
-                        //       onPressed: () {},
-                        //       splashColor: Colors.white,
-                        //       elevation: 20,
-                        //       shape: RoundedRectangleBorder(
-                        //         borderRadius: BorderRadius.circular(50),
-                        //       ),
-                        //       color: Color(0xff3c6970),
-                        //       padding: EdgeInsets.all(30),
-                        //       child: Text(
-                        //         "Visitors List",
-                        //         style: TextStyle(
-                        //             fontSize: 15,
-                        //             color: Colors.white,
-                        //             fontWeight: FontWeight.bold),
-                        //       ),
-                        //     ),
-                        //   ),
-                        // ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 25),
+                          child: Container(
+                            width: 250,
+                            child: RaisedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => QrCode()));
+                              },
+                              splashColor: Colors.white,
+                              elevation: 20,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              color: Color(0xff3c6970),
+                              padding: EdgeInsets.all(30),
+                              child: Text(
+                                "Generate QR Code",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
                         SizedBox(
                           height: 5,
                         ),
