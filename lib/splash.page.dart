b6614b8e32841f5f11aacea171c747380690ee1e
Ziyad-Seif <ziyad.seif@ibm.com>
2022-09-23 13:53:27 +0200

    LOL

--- lib/splash.page.dart
+++ lib/splash.page.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
@@ -14,20 +13,3 @@ class SplashScreen extends StatelessWidget {
     );
   }
 }
-=======
-import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-
-class SplashScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(
-          child: SpinKitPulse(),
-        ),
-      ),
-    );
-  }
-}
->>>>>>> 88c264242aede7ffadc5c28d1b673476e018d33a
